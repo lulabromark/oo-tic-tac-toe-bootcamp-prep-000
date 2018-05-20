0e227254c0b92b4c1caed8f5ff6060fcb5ae0054
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-20 00:29:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,4 +56,12 @@ class TicTacToe
     end
   end
 
+  def turn_count
+    counter = 0
+    @board.each do |turn|
+      counter += 1 if turn == 'X' || turn == 'O'
+    end
+    counter
+  end
+
 end
